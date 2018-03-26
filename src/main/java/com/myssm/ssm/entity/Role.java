f91557953e6daaa4d04509b1266cb5204b4e2821
600837 <600837@syswin.com>
2018-03-26 17:10:56 +0800

    角色类创建

--- src/main/java/com/myssm/ssm/entity/Role.java
+++ src/main/java/com/myssm/ssm/entity/Role.java
@@ -4,5 +4,40 @@ package com.myssm.ssm.entity;
  * Created by liucheng on 2018/3/13.
  */
 public class Role {
+    /**
+     * 角色编号
+     */
+    String roleId;
+    /**
+     * 角色名称
+     */
+    String roleName;
+    /**
+     * 角色等级
+     */
+    String roleLevel;
 
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleLevel() {
+        return roleLevel;
+    }
+
+    public void setRoleLevel(String roleLevel) {
+        this.roleLevel = roleLevel;
+    }
 }
